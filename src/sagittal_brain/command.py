5463c606530c1ff37e3e25e85db94dd60425c1f7
YlF <ucapaci@ucl.ac.uk>
2025-11-17 11:40:15 +0000

    lib36

---
+++ src/sagittal_brain/command.py
@@ -0,0 +1,29 @@
+# src/sagittal_brain/command.py
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from .sagittal_brain import run_averages
+
+
+def main():
+    """Entry point for the sagittal_average_run command"""
+    parser = ArgumentParser(
+        description="Calculates the average for each sagittal-horizontal plane.",
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        'file_input', 
+        nargs='?', 
+        default="brain_sample.csv",
+        help="Input CSV file with the results from scikit-brain binning algorithm."
+    )
+    parser.add_argument(
+        '--file_output', '-o', 
+        default="brain_average.csv",
+        help="Name of the output CSV file."
+    )
+    arguments = parser.parse_args()
+
+    run_averages(arguments.file_input, arguments.file_output)
+
+
+if __name__ == "__main__":
+    main()
